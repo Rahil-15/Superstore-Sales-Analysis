4064ad0ff9ebb87c35f01889a0c1e94a36605616
rahil <rahiljc15@gmail.com>
2025-08-19 15:57:29 +0530

    Superstore Code

---
+++ demo.py
@@ -0,0 +1,16 @@
+fruits=['apple','mango','banana']
+print(fruits)
+print( )
+
+name=('Sahana','Veer','Jeevan','Veer')
+print(name)
+print(name[1])
+print(len(name))
+print(type(name))
+print( )
+
+college={'KLEIT','JAIN','GIT'}
+print(college)
+
+college_names={'JSS','RV','KLE'}
+print(college_names)
